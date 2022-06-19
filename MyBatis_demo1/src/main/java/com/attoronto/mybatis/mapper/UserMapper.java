1673cfc86e176ecc78ae51db181bfbf0d99c6894
Bruce Tang <brucetang6@gmail.com>
2022-06-19 01:30:51 -0400

    1st

---
+++ MyBatis_demo1/src/main/java/com/attoronto/mybatis/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.attoronto.mybatis.mapper;
+
+public interface UserMapper {
+    /**
+     * MyBatis 面向接口编程的两个一致：
+     * 1. 映射文件的namespace要和mapper接口的全类名保持一致
+     * 2. 映射文件中的SQL语句的id要和mapper接口中的方法名一致
+     *
+     * 表-- 实体类--mapper接口--映射文件
+     */
+
+    /**
+     * 添加用户信息
+     */
+    int insertUser();
+
+
+}
---
+++ MyBatis_demo1/src/test/java/com/attoronto/mybatis/test/MyBatisTest.java
@@ -0,0 +1,38 @@
+package com.attoronto.mybatis.test;
+
+import com.attoronto.mybatis.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisTest {
+
+    /**
+     * SqlSessionManager 默认不自动提交事务， 若需要自动提交事务
+     *  可以使用SqlSessionFactory.openSession(true);
+     */
+    @Test
+    public void testMyBatis() throws IOException {
+        //加载核心配置文件
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        //获取SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        //获取SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+        //获取SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        //获取mapper接口对象
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        //测试功能
+        int result = mapper.insertUser();
+        //提交事务
+     //   sqlSession.commit();
+        System.out.println("result：  "+result);
+    }
+}
